57773324a6868bab400afc94cf3cc6aa87de075c
danielclough <danielclough@gmail.com>
2020-12-07 21:28:15 -0800

    init Kiki's Quilts

--- src/components/IconMenu.js
+++ src/components/IconMenu.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FaCircle, FaPaintBrush, FaGavel, FaBolt, FaHandPointer } from 'react-icons/fa';
 import { FormattedMessage } from 'react-intl';
+import PreviewImage from '../components/PreviewCompatibleImage'
 
 const IconMenu = class extends React.Component {
 
---
+++ tmp/tmp.md
@@ -0,0 +1,7 @@
+# Add SVG components: 
+https://www.gatsbyjs.com/plugins/gatsby-plugin-react-svg/?=svg
+
+import Icon from "./path/assets/icon.svg";
+// ...
+<Icon />;
+
